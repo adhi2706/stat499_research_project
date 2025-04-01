66919885caaec6111edbb18ba3d28705806e8fa7
adhi2706 <adhigunap@gmail.com>
2025-03-31 20:47:03 -0600

    Added code for skewed alpha parameters

--- bayes_conformity_score_simulation.R
+++ bayes_conformity_score_simulation.R
@@ -161,7 +161,7 @@ validity_simulation <- function(category_lim, error_prob, n) {
     
     coverages <- numeric()
     
-    # Repeat 200 times for fixed K
+    # Repeat 500 MC simulations for fixed K
     for (sim_rep in 1:500) {
       
       alpha <- runif(K, min = 0.1, max = 10)
@@ -191,38 +191,19 @@ validity_simulation <- function(category_lim, error_prob, n) {
   return(coverage_rates)
 }
 
-n <- 2000 # Number of observations in calibration set
-epsilon <- 0.10 # User-specified error probability
-category_lim <- 20
-num_test_obs <- 500
-# k <- 15 # Number of categories
-# Y <- t(sapply(1:n, function(i) generate_one_hot(k)))
-
-# Run the simulation
-results <- validity_simulation(category_lim, error_prob = epsilon, n)
-
-# Plot the results
-plot(3:category_lim, results, 
-     type = 'b', 
-     xlab = 'Number of Categories (K)', 
-     ylab = 'Coverage Rate', 
-     main = 'Coverage rate of split conformal prediction using Bayes Conformity Score',
-     ylim = c(0.8, 1))
-abline(h = 1 - epsilon, col = 'red', lty = 2)
-
 
 # Plot coverage rates for different sizes of calibration set
 category_lim <- 20
 epsilon <- 0.10
-n_values <- c(500, 1000, 2000)  # Different values of n
-colors <- c("blue", "green", "black")  # Colors for the lines
+n_values <- c(500, 1000, 2000)
+colors <- c("blue", "green", "black")
 
 plot(NULL, xlim = c(3, category_lim), ylim = c(0.85, 1), 
      xlab = 'Number of Categories (K)', 
      ylab = 'Coverage Rate', 
      main = 'Coverage rate of split conformal prediction using Bayes Conformity Score')
 
-abline(h = 1 - epsilon, col = 'red', lty = 2)  # Target coverage rate
+abline(h = 1 - epsilon, col = 'red', lty = 2)
 
 for (i in seq_along(n_values)) {
   n <- n_values[i]
@@ -233,20 +214,44 @@ for (i in seq_along(n_values)) {
 legend("bottomright", legend = paste("n =", n_values), col = colors, lty = 1, pch = 16)
 
 
+# Checking coverage for large K
+
+n <- 3000 # Number of observations in calibration set
+epsilon <- 0.10 # User-specified error probability
+category_lim <- 50
+# k <- 15 # Number of categories
+# Y <- t(sapply(1:n, function(i) generate_one_hot(k)))
+
+# Run the simulation
+results <- validity_simulation(category_lim, error_prob = epsilon, n)
+
+# Plot the results
+plot(3:category_lim, results, 
+     type = 'b', 
+     col = "blue",
+     xlab = 'Number of Categories (K)', 
+     ylab = 'Coverage Rate', 
+     main = 'Coverage rate of split conformal prediction using Bayes Conformity Score',
+     xlim = c(20, category_lim),
+     ylim = c(0.85, 1))
+abline(h = 1 - epsilon, col = 'red', lty = 2)
+
+
+
 ##### Comparison of size of prediction set from different conformity scores #####
 
 comparision_by_category <- function(category_lim, error_prob, n) {
   
-  bayes_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 500)
-  dta_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 500)
+  bayes_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 1000)
+  dta_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 1000)
   
   for (K in 3:category_lim) {
     
-    # Repeat 500 times for fixed K
-    for (sim_rep in 1:500) {
+    # Repeat 1000 MC simulations for fixed K
+    for (sim_rep in 1:1000) {
       
       alpha <- runif(K, min = 0.1, max = 1)
-      #alpha <- (rep(0.01,K))
+      #alpha <- (rep(1,K))
       
       class_prob <- rdirichlet(1, alpha)[1,]
       Y <- t(rmultinom(n, 1, class_prob))
@@ -296,4 +301,62 @@ category_lim <- 20
 
 # Run the simulation
 compare_results <- comparision_by_category(category_lim, error_prob = epsilon, n)
+plot_prediction_set_sizes(compare_results, category_lim)
+
+
+# Function to produce a skewed Dirichlet concentration parameter vector
+create_skewed_alpha <- function(K, skew_ratio) {
+  # Initialize an empty vector for alpha
+  alpha <- numeric(K)
+  
+  denominator <- (0.3 * skew_ratio + 0.7) * K
+  # Set 30% of entries to higher value
+  high_count <- round(0.3 * K)
+  high_indices <- sample(1:K, high_count)
+  
+  # Assign values
+  #alpha[high_indices] <- skew_ratio/denominator
+  #alpha[-high_indices] <- 1/denominator
+  alpha[high_indices] <- 1
+  alpha[-high_indices] <- skew_ratio
+  
+  return(alpha)
+}
+
+
+compare_skewed_prior <- function(category_lim, error_prob, n, skew_ratio) {
+  
+  bayes_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 1000)
+  dta_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 1000)
+  
+  for (K in 3:category_lim) {
+    
+    # Repeat 1000 times for fixed K
+    for (sim_rep in 1:1000) {
+      
+      true_alpha <- create_skewed_alpha(K, skew_ratio)
+      class_prob <- rdirichlet(1, true_alpha)[1,]
+      Y <- t(rmultinom(n, 1, class_prob))
+      
+      assumed_alpha <- true_alpha
+      result_bayes <- bayes_split_cp(Y, K, assumed_alpha, error_prob)
+      result_dta <- dta_split_cp(Y, K, error_prob)
+      
+      bayes_prediction_sizes[K-2, sim_rep] <- nrow(result_bayes$prediction_set)
+      dta_prediction_sizes[K-2, sim_rep] <- nrow(result_dta$prediction_set)
+      
+    }
+  }
+  
+  return(list(bayes_sizes = apply(bayes_prediction_sizes, 1, mean),
+              dta_sizes = apply(dta_prediction_sizes, 1, mean) ))
+}
+
+
+n <- 100 # Number of observations in calibration data
+epsilon <- 0.1 # User-specified error probability
+category_lim <- 50
+
+# Run the simulation
+compare_results <- compare_skewed_prior(category_lim, error_prob = epsilon, n, 20)
 plot_prediction_set_sizes(compare_results, category_lim)
