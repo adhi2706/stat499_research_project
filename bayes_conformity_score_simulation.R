6dcb3f2971a693b6b297fb4ae32d3f52848a8501
adhi2706 <adhigunap@gmail.com>
2025-04-02 23:57:05 -0600

    Code updates

--- bayes_conformity_score_simulation.R
+++ bayes_conformity_score_simulation.R
@@ -140,8 +140,8 @@ validity_simulation <- function(category_lim, error_prob, n) {
   for (K in 3:category_lim) {
     coverages <- numeric()
     
-    # Repeat 1000 MC simulations for fixed K
-    for (sim_rep in 1:1000) {
+    # Repeat 2000 MC simulations for fixed K
+    for (sim_rep in 1:2000) {
       
       # Generate alpha for Dirichlet prior
       alpha <- runif(K, min=0.1, max=10)
@@ -174,7 +174,7 @@ validity_simulation <- function(category_lim, error_prob, n) {
 # Plot coverage rates for different sizes of calibration set
 category_lim <- 20
 epsilon <- 0.10
-n_values <- c(50, 100, 200)
+n_values <- c(20, 50, 100)
 colors <- c("blue", "green", "black")
 
 plot(NULL, xlim = c(3, category_lim), ylim = c(0.85, 1), 
@@ -195,11 +195,9 @@ legend("bottomright", legend = paste("n =", n_values), col = colors, lty = 1, pc
 
 # Checking coverage for large K
 
-n <- 50 # Number of observations in calibration set
+n <- 20 # Number of observations in calibration set
 epsilon <- 0.10 # User-specified error probability
 category_lim <- 50
-# k <- 15 # Number of categories
-# Y <- t(sapply(1:n, function(i) generate_one_hot(k)))
 
 # Run the simulation
 results <- validity_simulation(category_lim, error_prob = epsilon, n)
@@ -212,7 +210,8 @@ plot(3:category_lim, results,
      ylab = 'Coverage Rate', 
      main = 'Coverage Rate of Full conformal prediction using Bayes Conformity Score',
      xlim = c(20, category_lim),
-     ylim = c(0.85, 1))
+     ylim = c(0.85, 1),
+     pch = 16)
 abline(h = 1 - epsilon, col = 'red', lty = 2)
 
 
@@ -221,16 +220,16 @@ abline(h = 1 - epsilon, col = 'red', lty = 2)
 
 comparision_by_category <- function(category_lim, error_prob, n) {
   
-  bayes_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 1000)
-  dta_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 1000)
+  bayes_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 2000)
+  dta_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 2000)
   
   for (K in 3:category_lim) {
     
-    # Repeat 1000 MC simulations for fixed K
-    for (sim_rep in 1:1000) {
+    # Repeat 2000 MC simulations for fixed K
+    for (sim_rep in 1:2000) {
       
-      alpha <- runif(K, min = 0.1, max = 1)
-      #alpha <- (rep(1,K))
+      #alpha <- runif(K, min = 0.1, max = 1)
+      alpha <- (rep(1,K))
       
       class_prob <- rdirichlet(1, alpha)[1,]
       Y <- t(rmultinom(n, 1, class_prob))
@@ -256,7 +255,7 @@ plot_prediction_set_sizes <- function(results, category_lim) {
   plot(3:category_lim, results$bayes_sizes, 
        type = 'b', 
        col = 'blue', 
-       xlab = 'Number of Categories', 
+       xlab = 'Number of Categories (K)', 
        ylab = 'Average Prediction Set Size',
        main = 'Comparison of Prediction Set Sizes by Category Count',
        ylim = c(0, max(c(results$bayes_sizes, results$dta_sizes)) * 1.2))
@@ -303,15 +302,16 @@ create_skewed_alpha <- function(K, skew_ratio) {
 }
 
 
-compare_skewed_prior <- function(category_lim, error_prob, n, skew_ratio) {
+# Comparing prediction sets when skewed prior assumption is true
+compare_skewed_prior_true <- function(category_lim, error_prob, n, skew_ratio) {
   
-  bayes_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 1000)
-  dta_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 1000)
+  bayes_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 3000)
+  dta_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 3000)
   
   for (K in 3:category_lim) {
     
-    # Repeat 1000 times for fixed K
-    for (sim_rep in 1:1000) {
+    # Repeat 3000 times for fixed K
+    for (sim_rep in 1:3000) {
       
       true_alpha <- create_skewed_alpha(K, skew_ratio)
       class_prob <- rdirichlet(1, true_alpha)[1,]
@@ -332,10 +332,103 @@ compare_skewed_prior <- function(category_lim, error_prob, n, skew_ratio) {
 }
 
 
+n <- 15 # Number of observations in calibration data
+epsilon <- 0.1 # User-specified error probability
+category_lim <- 20
+
+# Run the simulation
+compare_results <- compare_skewed_prior_true(category_lim, error_prob = epsilon, n, 4)
+plot_prediction_set_sizes(compare_results, category_lim)
+
+
 n <- 50 # Number of observations in calibration data
 epsilon <- 0.1 # User-specified error probability
 category_lim <- 20
 
 # Run the simulation
-compare_results <- compare_skewed_prior(category_lim, error_prob = epsilon, n, 4)
+compare_results <- compare_skewed_prior_true(category_lim, error_prob = epsilon, n, 4)
+plot_prediction_set_sizes(compare_results, category_lim)
+
+
+
+# Function to assigns higher alpha_j to categories different from true alpha used to generate data
+create_incorrect_skew <- function(alpha) {
+  unique_values <- unique(alpha)
+  
+  if(length(unique_values) <= 1) {
+    return(alpha)
+  }
+  
+  sorted_values <- sort(unique_values)
+  high_value <- sorted_values[length(sorted_values)]
+  low_value <- sorted_values[1]
+  
+  high_indices <- which(alpha == high_value)
+  low_indices <- which(alpha == low_value)
+  n_high <- length(high_indices)
+  n_low <- length(low_indices)
+  
+  # Create a new alpha vector with shuffled categories
+  new_alpha <- alpha
+  new_high_indices <- sample(low_indices, n_high)
+  new_alpha[] <- low_value
+  new_alpha[new_high_indices] <- high_value
+  
+  # Check if the new assignment is the same as the original
+  while(all(new_alpha == alpha)) {
+    new_high_indices <- sample(low_indices, n_high)
+    new_alpha[] <- low_value
+    new_alpha[new_high_indices] <- high_value
+  }
+  
+  return(new_alpha)
+}
+
+
+# Comparing prediction sets when skewed prior assumption is false
+compare_skewed_prior_false <- function(category_lim, error_prob, n, skew_ratio) {
+  
+  bayes_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 3000)
+  dta_prediction_sizes <- matrix(0, nrow = category_lim - 2, ncol = 3000)
+  
+  for (K in 3:category_lim) {
+    
+    # Repeat 3000 times for fixed K
+    for (sim_rep in 1:3000) {
+      
+      true_alpha <- create_skewed_alpha(K, skew_ratio)
+      #true_alpha <- (rep(0.1,K))
+      class_prob <- rdirichlet(1, true_alpha)[1,]
+      Y <- t(rmultinom(n, 1, class_prob))
+      
+      assumed_alpha <- create_incorrect_skew(true_alpha)
+      #assumed_alpha <- create_skewed_alpha(K, skew_ratio)
+      result_bayes <- bayes_full_cp(Y, K, assumed_alpha, error_prob)
+      result_dta <- dta_full_cp(Y, K, error_prob)
+      
+      bayes_prediction_sizes[K-2, sim_rep] <- nrow(result_bayes$prediction_set)
+      dta_prediction_sizes[K-2, sim_rep] <- nrow(result_dta$prediction_set)
+      
+    }
+  }
+  
+  return(list(bayes_sizes = apply(bayes_prediction_sizes, 1, mean),
+              dta_sizes = apply(dta_prediction_sizes, 1, mean) ))
+}
+
+n <- 15 # Number of observations in calibration data
+epsilon <- 0.1 # User-specified error probability
+category_lim <- 20
+
+# Run the simulation
+compare_results <- compare_skewed_prior_false(category_lim, error_prob = epsilon, n, 8)
+plot_prediction_set_sizes(compare_results, category_lim)
+
+
+n <- 50 # Number of observations in calibration data
+epsilon <- 0.1 # User-specified error probability
+category_lim <- 20
+
+# Run the simulation
+compare_results <- compare_skewed_prior_false(category_lim, error_prob = epsilon, n, 4)
 plot_prediction_set_sizes(compare_results, category_lim)
